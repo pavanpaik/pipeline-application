b4f830c9fbc6383b6732d43d15b1d41fb945d788
pavankumarpai@gmail.com <pavankumarpai@gmail.com>
2018-06-04 10:51:53 -0400

    more monkeys

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	log.Println("Starting pipeline application...")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello monkey!\n")
+		fmt.Fprintf(w, "Hello monkeys!\n")
 	})
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
